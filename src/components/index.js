0b34572df3aa2bf92c3689744a738cf97eb412ed
Software0408 <muhammadtoha2019@gmai.com>
2024-07-07 18:32:44 +0100

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Article} from './article/Article';
+export {default as Brand} from './brand/Brand';
+export {default as CTA} from './callToAction/CTA';
+export {default as feature} from './feature/Feature';
+export {default as NavBar} from './navbar/NavBar';
